f6dcd936cb5cfb1827ba3f29a065c40ce95885ff
Chris Yammine <chris.yammine@hotmail.com>
2014-05-24 16:09:43 -0700

    Add database integration and home page display

--- app.rb
+++ app.rb
@@ -5,12 +5,23 @@ DataMapper.setup(:default, ENV["DATABASE_URL"] || "sqlite3://#{Dir.pwd}/contract
 
 class Snippet
 
+  def self.number_of_kind(kind)
+    x = 0
+    self.all.each do |snip|
+      if snip.kind.include?(kind)
+        x += 1
+      end
+    end
+    x
+  end
+
   include DataMapper::Resource
 
-  property :id, Serial
-  property :kind, String
-  property :title, String
-  property :work, Text
+  property :id,     Serial
+
+  property :kind,   String
+  property :title,  String
+  property :work,   Text
 
 end
 
@@ -19,9 +30,23 @@ DataMapper.finalize
 Snippet.auto_upgrade!
 
 get '/' do
+  @snippets = Snippet.all 
+  @ruby = Snippet.number_of_kind 'Ruby'
+  @js = Snippet.number_of_kind 'JavaScript'
+  @cs = Snippet.number_of_kind 'CoffeeScript'
+  @html = Snippet.number_of_kind 'HTML'
+  @css = Snippet.number_of_kind 'CSS'
+  
   erb :index, layout: :default_layout
 end
 
 get '/add' do
   erb :add_snippet, layout: :default_layout
+end
+
+post '/add' do
+  Snippet.create(params)
+
+  redirect to('/')
+
 end
